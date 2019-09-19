c9e19677021fad29e2956b52d17acd5304acf08c
Keshav <kshavgupta47@gmail.com>
2019-09-19 13:44:12 +0530

    aaaa

--- code/model.py
+++ code/model.py
@@ -286,12 +286,11 @@ class CA_NET(nn.Module):
 
     def reparametrize(self, mu, logvar):
         std = logvar.mul(0.5).exp_()
-        if cfg.CUDA:
-            eps = torch.cuda.FloatTensor(std.size()).normal_()
-        else:
-            eps = torch.FloatTensor(std.size()).normal_()
+        # if cfg.CUDA:
+        #     eps = torch.cuda.FloatTensor(std.size()).normal_()
+        # else:
+        eps = torch.FloatTensor(std.size()).normal_()
         eps = Variable(eps)
-        mu = mu.to('cuda')
         return eps.mul(std).add_(mu)
 
     def forward(self, text_embedding):
